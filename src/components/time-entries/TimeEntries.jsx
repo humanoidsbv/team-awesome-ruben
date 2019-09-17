726c8033b6dfd2b545a865e2a4f127f31ed3ee22
Ruben Werdmuller <rubenwerdmuller@gmail.com>
2019-09-17 11:53:37 +0200

    Create time-entry modules

--- src/components/time-entries/TimeEntries.jsx
+++ src/components/time-entries/TimeEntries.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import styles from './TimeEntries.module.css';
 
 import TimeEntry from '../time-entry/TimeEntry';
 import TimeEntryAdd from '../time-entry-add/TimeEntryAdd';
+import TimeStamp from '../time-stamp/TimeStamp';
+import styles from './TimeEntries.module.css';
 
 const TimeEntries = () => (
   <div className={styles.timeEntries}>
     <TimeEntryAdd />
+    <TimeStamp />
     <TimeEntry />
+    <TimeStamp />
     <TimeEntry />
     <TimeEntry />
   </div>
--- src/components/time-entry-add/TimeEntryAdd.jsx
+++ src/components/time-entry-add/TimeEntryAdd.jsx
@@ -2,54 +2,88 @@ import React from 'react';
 
 import styles from './TimeEntryAdd.module.css';
 
+import IconPlus from '../../assets/icons/icon-plus.svg';
+
 const TimeEntryAdd = () => (
-  <div className={styles.timeEntryAdd}>
-    <button type="button" className={styles.closeButton}>
-      x
-    </button>
+  <div className={styles.container}>
     <form className={styles.form}>
-      <label className={styles.label} htmlFor="employer">
-        <p className={styles.labelText}>EMPLOYER</p>
-        <select className={styles.select} id="employer">
+      <button type="button" className={styles.closeButton}>
+        <IconPlus />
+      </button>
+
+      <label
+        className={`${styles.label} ${styles.labelLarge}`}
+        htmlFor="client"
+      >
+        <p className={styles.labelText}>CLIENT</p>
+        <select className={styles.select} id="client">
           <option value="">Port of Rotterdam</option>
           <option value="">Hike One</option>
         </select>
       </label>
 
-      <label className={styles.label} htmlFor="activity">
+      <label
+        className={`${styles.label} ${styles.labelLarge}`}
+        htmlFor="activity"
+      >
         <p className={styles.labelText}>ACTIVITY</p>
         <select className={styles.select} id="activity">
           <option value="">Design</option>
         </select>
       </label>
-
-      <label className={styles.label} htmlFor="date">
+      <label
+        className={`${styles.label} ${styles.labelMedium} ${styles.date}`}
+        htmlFor="date"
+      >
         <p className={styles.labelText}>DATE</p>
-        <select className={styles.select} id="date">
-          <option value="">29-07-2018</option>
-        </select>
+        <input
+          type="date"
+          className={styles.select}
+          id="date"
+          defaultValue="2018-07-29"
+        ></input>
       </label>
 
       <label
-        className={`${styles.label} ${styles.labelHalfWidth}`}
+        className={`
+          ${styles.label}
+          ${styles.labelSmall} 
+          ${styles.labelHalfWidth}
+        `}
         htmlFor="from"
       >
         <p className={styles.labelText}>FROM</p>
-        <select className={styles.select} id="from">
-          <option value="">09:00</option>
-        </select>
+        <input
+          type="time"
+          defaultValue="09:00"
+          className={`${styles.select} ${styles.time}`}
+          id="from"
+        />
       </label>
-
       <label
-        className={`${styles.label} ${styles.labelHalfWidth}`}
+        className={`
+          ${styles.label}
+          ${styles.labelSmall} 
+          ${styles.labelHalfWidth}
+        `}
         htmlFor="to"
       >
         <p className={styles.labelText}>TO</p>
-        <select className={styles.select} id="to">
-          <option value="">17:00</option>
-        </select>
+        <input
+          type="time"
+          defaultValue="17:00"
+          className={`${styles.select} ${styles.time}`}
+          id="to"
+        />
       </label>
+      <button className={styles.addButton}>
+        <p>Add</p>
+      </button>
     </form>
+    <button className={styles.showForm}>
+      <IconPlus className={styles.showFormIcon} />
+      <p>New time entry</p>
+    </button>
   </div>
 );
 
