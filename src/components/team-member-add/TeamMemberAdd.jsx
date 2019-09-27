5e4e2a6b5de28c9428bb8924b03f3d9b8f1860f0
Ruben Werdmuller <rubenwerdmuller@gmail.com>
2019-09-27 17:14:11 +0200

    Add team members form

--- src/components/team-member-add/TeamMemberAdd.jsx
+++ src/components/team-member-add/TeamMemberAdd.jsx
@@ -1,15 +1,126 @@
 import React from 'react';
 
-import IconPlus from '../../assets/icons/icon-plus.svg';
 import styles from './TeamMemberAdd.module.css';
+import UserProfileImage from '../../../static/images/picture-tnt.jpg';
 
 const TeamMemberAdd = () => {
   return (
-    <div className={styles.container}>
-      <button className={styles.showForm} type="button">
-        <IconPlus className={styles.showFormIcon} />
-        New Humanoid
-      </button>
+    <div>
+      <div className={styles.header}>
+        <span className={styles.heading}>Add new team member</span>
+        <button type="button" className={styles.cancelButton}>
+          Cancel
+        </button>
+        <button type="button" className={styles.addMemberButton}>
+          Save
+        </button>
+      </div>
+      <div className={styles.formTab}>
+        <span>Personal Details</span>
+      </div>
+      <form className={styles.form}>
+        <div className={styles.profileImageContainer}>
+          <img
+            alt="Profile avatar"
+            className={styles.profileImage}
+            src={UserProfileImage}
+          />
+          <span>Edit Avatar</span>
+        </div>
+        <div className={styles.inputContainer}>
+          <label
+            className={`${styles.label} ${styles.labelHalfWidth}`}
+            htmlFor="firstName"
+          >
+            <p className={styles.labelText}>First Name</p>
+            <input
+              className={styles.input}
+              id="firstName"
+              maxLength="30"
+              minLength="2"
+              name="firstName"
+              required
+            />
+          </label>
+          <label
+            className={`${styles.label} ${styles.labelHalfWidth}`}
+            htmlFor="lastName"
+          >
+            <p className={styles.labelText}>Last Name</p>
+            <input
+              className={styles.input}
+              id="lastName"
+              maxLength="30"
+              minLength="2"
+              name="lastName"
+              required
+            />
+          </label>
+          <label className={`${styles.label}`} htmlFor="e-mail">
+            <p className={styles.labelText}>E-mail Adress</p>
+            <input
+              className={styles.input}
+              id="e-mail"
+              maxLength="30"
+              minLength="2"
+              name="e-mail"
+              required
+            />
+          </label>
+          <label className={`${styles.label}`} htmlFor="bio">
+            <p className={styles.labelText}>Bio</p>
+            <textarea
+              className={`${styles.input} ${styles.textArea}`}
+              id="bio"
+              minLength="2"
+              name="bio"
+              required
+            />
+          </label>
+        </div>
+        <span className={styles.verticalLine} />
+        <div className={styles.inputContainer}>
+          <label className={`${styles.label}`} htmlFor="Adress">
+            <span className={styles.labelText}>Adress</span>
+            <input
+              className={styles.input}
+              id="Adress"
+              maxLength="30"
+              minLength="2"
+              name="Adress"
+              required
+            />
+          </label>
+          <label
+            className={`${styles.label} ${styles.labelHalfWidth}`}
+            htmlFor="zip-code"
+          >
+            <p className={styles.labelText}>ZIP code</p>
+            <input
+              className={styles.input}
+              id="zip-code"
+              maxLength="30"
+              minLength="2"
+              name="zip-code"
+              required
+            />
+          </label>
+          <label
+            className={`${styles.label} ${styles.labelHalfWidth}`}
+            htmlFor="city"
+          >
+            <p className={styles.labelText}>City</p>
+            <input
+              className={styles.input}
+              id="city"
+              maxLength="30"
+              minLength="2"
+              name="city"
+              required
+            />
+          </label>
+        </div>
+      </form>
     </div>
   );
 };
--- src/components/team-members/TeamMembers.jsx
+++ src/components/team-members/TeamMembers.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import IconArrowDown from '../../assets/icons/icon-arrow-down.svg';
+import IconPlus from '../../assets/icons/icon-plus.svg';
 import styles from './TeamMembers.module.css';
 import TeamMember from '../team-member/';
 import TeamMemberAdd from '../team-member-add/';
@@ -8,9 +9,13 @@ import TeamMemberAdd from '../team-member-add/';
 const TeamMembers = () => {
   return (
     <React.Fragment>
+      <TeamMemberAdd />
       <div className={styles.header}>
         <span className={styles.heading}>All Humanoids</span>
-        <TeamMemberAdd />
+        <button type="button" className={styles.addMember}>
+          <IconPlus className={styles.addNewIcon} />
+          New Humanoid
+        </button>
         <button type="button" className={styles.sortMembers}>
           Sort by:
           <IconArrowDown />
